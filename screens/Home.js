48663bdf1b5a63db76c72b1a6e22de01e3949929
Luigi-Travi-Raya <traviraya@gmail.com>
2022-08-04 10:51:51 -0300

    Update home cards

--- screens/Home.js
+++ screens/Home.js
@@ -13,29 +13,100 @@ export default function Home({ navigation }) {
           </Text>
         </Pressable>
       </View>
-
-      <View style={tailwind("bg-red-800 flex px-5 py-3 mt-5 ")}>
+      
+      <View style={tailwind("bg-red-600 flex px-5 py-3 mt-5 ")}>
         <View style={tailwind("flex flex-row justify-between")}>
           <Text style={tailwind("w-full font-normal text-white text-lg")}>
             Nome Tarefa
           </Text>
-          <View style={tailwind("flex")}>
-            <Pressable style={tailwind("flex p-2 ml-4 mr-2 border-2 rounded  ")} onPress={() => navigation.navigate("CreateEdit")}>
-              <ImageBackground source={{uri: 'https://reactjs.org/logo-og.png'}}  style={tailwind("self-center font-normal text-white text-sm")}/>
+          <View style={tailwind("flex flex-row mb-3")}>
+            <Pressable style={tailwind("flex p-2 ml-1 border-white bg-gray-200 rounded  ")} onPress={() => navigation.navigate("CreateEdit")}>
+              {/* <ImageBackground  source={require('../src/images/edit_icon.png')}  style={tailwind("self-center font-normal text-white text-sm")}/> */}
+              <Text style={tailwind("text-xs text-blue-500")}>
+                Edit
+              </Text>
+            </Pressable>
+            <Pressable style={tailwind("flex p-2 ml-1 mr-2 2 border-white bg-blue-500 rounded  ")} onPress={() => navigation.navigate("CreateEdit")}>
+              {/* <ImageBackground  source={require('../src/images/edit_icon.png')}  style={tailwind("self-center font-normal text-white text-sm")}/> */}
+              <Text style={tailwind("text-xs text-white")}>
+                Delete
+              </Text>
             </Pressable>
           </View>
         </View>
+
         <View style={tailwind("flex flex-row justify-between")}>
-          <Text style={tailwind("font-light bg-white text-gray-300 text-sm")}>
+          <Text style={tailwind("font-light text-gray-300 text-sm")}>
             Descrição Tarefa
           </Text>
           <Text style={tailwind("text-right font-light text-gray-300 text-sm")}>
             Data: xx/xx/xxxx
           </Text>
         </View>
-        <View style={tailwind("")}>
-
+        
+      </View>
+      <View style={tailwind("bg-yellow-600 flex px-5 py-3 mt-5 ")}>
+        <View style={tailwind("flex flex-row justify-between")}>
+          <Text style={tailwind("w-full font-normal text-white text-lg")}>
+            Nome Tarefa
+          </Text>
+          <View style={tailwind("flex flex-row mb-3")}>
+            <Pressable style={tailwind("flex p-2 ml-1 border-white bg-gray-200 rounded  ")} onPress={() => navigation.navigate("CreateEdit")}>
+              {/* <ImageBackground  source={require('../src/images/edit_icon.png')}  style={tailwind("self-center font-normal text-white text-sm")}/> */}
+              <Text style={tailwind("text-xs text-blue-500")}>
+                Edit
+              </Text>
+            </Pressable>
+            <Pressable style={tailwind("flex p-2 ml-1 mr-2 2 border-white bg-blue-500 rounded  ")} onPress={() => navigation.navigate("CreateEdit")}>
+              {/* <ImageBackground  source={require('../src/images/edit_icon.png')}  style={tailwind("self-center font-normal text-white text-sm")}/> */}
+              <Text style={tailwind("text-xs text-white")}>
+                Delete
+              </Text>
+            </Pressable>
+          </View>
         </View>
+
+        <View style={tailwind("flex flex-row justify-between")}>
+          <Text style={tailwind("font-light text-gray-300 text-sm")}>
+            Descrição Tarefa
+          </Text>
+          <Text style={tailwind("text-right font-light text-gray-300 text-sm")}>
+            Data: xx/xx/xxxx
+          </Text>
+        </View>
+        
+      </View>
+      
+      <View style={tailwind("bg-green-600 flex px-5 py-3 mt-5 ")}>
+        <View style={tailwind("flex flex-row justify-between")}>
+          <Text style={tailwind("w-full font-normal text-white text-lg")}>
+            Nome Tarefa
+          </Text>
+          <View style={tailwind("flex flex-row mb-3")}>
+            <Pressable style={tailwind("flex p-2 ml-1 border-white bg-gray-200 rounded  ")} onPress={() => navigation.navigate("CreateEdit")}>
+              {/* <ImageBackground  source={require('../src/images/edit_icon.png')}  style={tailwind("self-center font-normal text-white text-sm")}/> */}
+              <Text style={tailwind("text-xs text-blue-500")}>
+                Edit
+              </Text>
+            </Pressable>
+            <Pressable style={tailwind("flex p-2 ml-1 mr-2 2 border-white bg-blue-500 rounded  ")} onPress={() => navigation.navigate("CreateEdit")}>
+              {/* <ImageBackground  source={require('../src/images/edit_icon.png')}  style={tailwind("self-center font-normal text-white text-sm")}/> */}
+              <Text style={tailwind("text-xs text-white")}>
+                Delete
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+
+        <View style={tailwind("flex flex-row justify-between")}>
+          <Text style={tailwind("font-light text-gray-300 text-sm")}>
+            Descrição Tarefa
+          </Text>
+          <Text style={tailwind("text-right font-light text-gray-300 text-sm")}>
+            Data: xx/xx/xxxx
+          </Text>
+        </View>
+        
       </View>
     </SafeAreaView>
   );
