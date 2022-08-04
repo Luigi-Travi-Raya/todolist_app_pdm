0111d50845a5af397b6065c03488ce67931657f3
Leonardo Ramos <50186247+leomramos@users.noreply.github.com>
2022-08-04 00:11:23 -0300

    arrumado luigi...

--- screens/Home.js
+++ screens/Home.js
@@ -7,9 +7,9 @@ export default function Home({ navigation }) {
     <SafeAreaView style={tailwind("flex-1 bg-gray-900 ")}>
       <View style={tailwind("bg-indigo-300 px-5 py-5")}>
         {/* <Button style={tailwind("bg-indigo-500")} title="Nova Tarefa" onPress={() => navigation.navigate("CreateEdit")} /> */}
-        <Pressable style={tailwind("blockborder-solid border-2 border-white py-4 rounded-lg w-full bg-indigo-700 ")} onPress={() => navigation.navigate("CreateEdit")}>
-          <Text style={tailwind("self-center justify-self-center font-normal text-white text-lg")}>
-              Nova Tarefa
+        <Pressable style={tailwind("border-2 border-white py-4 rounded-lg w-full bg-indigo-700 ")} onPress={() => navigation.navigate("CreateEdit")}>
+          <Text style={tailwind("self-center font-normal text-white text-lg")}>
+            Nova Tarefa
           </Text>
         </Pressable>
       </View>
@@ -17,20 +17,20 @@ export default function Home({ navigation }) {
       <View style={tailwind("bg-red-800 flex px-5 py-3 mt-5 ")}>
         <View style={tailwind("flex mb-4")}>
           <Text style={tailwind("w-full font-normal text-white text-lg")}>
-              Nome Tarefa
+            Nome Tarefa
           </Text>
-          <View style={tailwind("self-right")}>
+          <View style={tailwind("flex")}>
             <Pressable style={tailwind("flex p-2 ml-4 mr-2 border-2 rounded  ")} onPress={() => navigation.navigate("CreateEdit")}>
-              <Text style={tailwind("self-center justify-self-center font-normal text-white text-lg")}> Editar </Text>
+              <Text style={tailwind("self-center font-normal text-white text-lg")}> Editar </Text>
             </Pressable>
           </View>
         </View>
-        <View style={tailwind("grid grid-cols-2")}>
-          <Text style={tailwind("self-left justify-self-start font-light bg-white text-gray-300 text-sm")}>
-              Descrição Tarefa daiowjdawidfjs ogiedjasrgo rgoaierjgaoiergj origj rgoijaergoisejrg oisedg eoisrgjseoirg
+        <View style={tailwind("flex flex-row justify-between")}>
+          <Text style={tailwind("font-light bg-white text-gray-300 text-sm")}>
+            Descrição Tarefa
           </Text>
-          <Text style={tailwind("text-right justify-self-start font-light text-gray-300 text-sm")}>
-              Data: xx/xx/xxxx
+          <Text style={tailwind("text-right font-light text-gray-300 text-sm")}>
+            Data: xx/xx/xxxx
           </Text>
         </View>
         <View style={tailwind("")}>
